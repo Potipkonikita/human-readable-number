02fab7d586e671567e022bd9d950ae6e3d6a292a
Nikita Potipko <potipkonikita@gmail.com>
2022-01-12 02:03:59 +0300

    docs:push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let res1 = "",
+        res2 = "";
+    const num1 = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const num2 = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const num3 = [
+        "",
+        "ten",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    sotni = Math.floor(number / 100);
+    dvuhzn = number % 100;
+    des = Math.floor(dvuhzn / 10);
+    ed = dvuhzn % 10;
+    if (number == 0) return "zero";
+    if (dvuhzn < 10) res2 = num1[ed];
+    if (dvuhzn >= 10 && dvuhzn < 20) res2 = num2[ed];
+    else if (dvuhzn >= 20 && ed != 0) res2 = num3[des] + " " + num1[ed];
+    else if (dvuhzn >= 20 && ed == 0) res2 = num3[des];
+    if (sotni > 0 && dvuhzn != 0) res1 = num1[sotni] + " hundred ";
+    else if (sotni > 0 && dvuhzn == 0) res1 = num1[sotni] + " hundred";
+    result = res1 + res2;
+    return result;
+};
