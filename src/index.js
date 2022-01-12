b9ac08976e992a9dce3136ddaaeee7f77544e8cc
Potipkonikita <93899955+Potipkonikita@users.noreply.github.com>
2022-01-12 20:15:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -37,17 +37,17 @@ module.exports = function toReadable(number) {
         "eighty",
         "ninety",
     ];
-    sotni = Math.floor(number / 100);
-    dvuhzn = number % 100;
-    des = Math.floor(dvuhzn / 10);
-    ed = dvuhzn % 10;
-    if (number == 0) return "zero";
+    let sotni = Math.floor(number / 100);
+    let dvuhzn = number % 100;
+    let des = Math.floor(dvuhzn / 10);
+    let ed = dvuhzn % 10;
+    if (number === 0) {return "zero";}
     if (dvuhzn < 10) res2 = num1[ed];
     if (dvuhzn >= 10 && dvuhzn < 20) res2 = num2[ed];
     else if (dvuhzn >= 20 && ed != 0) res2 = num3[des] + " " + num1[ed];
-    else if (dvuhzn >= 20 && ed == 0) res2 = num3[des];
+    else if (dvuhzn >= 20 && ed === 0) res2 = num3[des];
     if (sotni > 0 && dvuhzn != 0) res1 = num1[sotni] + " hundred ";
-    else if (sotni > 0 && dvuhzn == 0) res1 = num1[sotni] + " hundred";
-    result = res1 + res2;
+    else if (sotni > 0 && dvuhzn === 0) res1 = num1[sotni] + " hundred";
+    let result = res1 + res2;
     return result;
 };
